4c25d85e169c59346c8793e405d4624a6f7a2d2a
loolaali859 <121094141+loolaali859@users.noreply.github.com>
2022-12-27 13:51:26 +0200

    Add files via upload

---
+++ SING IN.php
@@ -0,0 +1,59 @@
+<html>
+    <head>
+        <title> login</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css\style.css">
+    </head>
+<body>
+    
+    <nav class="menu">
+     <ul>
+      <li><a href="http://localhost/market/radi.php">Home</a></li>    
+      <li><a href="http://localhost/market/gallery.php">Gallery</a></li> 
+      <li><a href="http://localhost/market/help.php">Help</a></li>
+      <li><a href="http://localhost/market/about.php">About</a></li>
+      <li><a href="http://localhost/market/COURS1.php">Sign in</a></li>
+     </ul>
+    
+ <form class="search-form" >
+     
+     <input type="text" placeholder="search">
+     <button>search</button>
+     
+ </form>
+       <img style="width: 100px;float: right;margin-right: 20px;" src="img/rg.png"  > 
+    </nav>
+     
+    <div class="box">
+        
+        <h2>Login</h2>
+        
+        <form action="" method="post">
+            
+            <div class="inputBox">
+             <input type="" name="" required="">
+             <label>Username</label>
+            </div>
+            <div class="inputBox" >
+                <input type="password" name="" required="">
+                <label>Password</label>
+                
+            </div>
+            <input type="submit" name="" value="Submit">
+        </form>
+    </div>
+
+    <div class="footer">
+      <table style="margin-left: 580px;">
+          <tr>
+              <td><a href="#"><h4 style="color: white;">call as   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">politics   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">privacy     </h4></a></td>
+          </tr>
+      </table>  
+      <p style="margin-left:603px;font-size: 10px;">جميع الحقوق محفوظة للدفعة 11 شبكات 2022</p>
+    </div>
+    
+    
+</body>    
+</html>
